c3dd4faefbcd6a13a597fada06b522e7ed91a894
VitaminC1000 <118882055+VitaminC1000@users.noreply.github.com>
2024-03-25 23:52:29 +0900

    Update 체육복.py

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,19 +1,21 @@
 def solution(n, lost, reserve):
-    # 정렬
+    answer = 0
+
     lost.sort()
     reserve.sort()
-	
-    # lost, reserve에 공통으로 있는 요소 제거
-    for i in reserve[:]:
-        if i in lost:
-            reserve.remove(i)
-            lost.remove(i)
-	
-    # 체육복 빌려주기(나의 앞 번호부터 확인)
-    for i in reserve:
-        if i-1 in lost:
-            lost.remove(i-1)
-        elif i+1 in lost:
-            lost.remove(i+1)
     
-    return n-len(lost)
+    for i in lost.copy():
+        if i in reserve:
+            lost.remove(i)
+            reserve.remove(i)
+            
+    for i in reserve:
+        if (i - 1) in lost:
+            lost.remove(i - 1)
+            
+        elif (i + 1) in lost:
+            lost.remove(i + 1)
+            
+    answer = n - len(lost)
+        
+    return answer
