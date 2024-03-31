bf4457323644ea87ad35839d61b22e4dc4bd834f
VitaminC1000 <118882055+VitaminC1000@users.noreply.github.com>
2024-03-31 19:51:05 +0900

    Update 수 조작하기 1.py

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,16 +1,3 @@
 def solution(n, control):
-    answer = []
-    
-    for i in control:
-        if i == "w":
-            answer.append(1)
-        
-        elif i == "s":
-            answer.append(-1)
-            
-        elif i == "d":
-            answer.append(10)
-            
-        elif i == "a":
-            answer.append(-10)
-    return sum(answer) + n
+    key = dict(zip(['w','s','d','a'], [1,-1,10,-10]))
+    return n + sum([key[c] for c in control])
