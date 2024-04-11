5ae387d431521b6f5ae0ba431823582ac9b1e9e0
VitaminC1000 <118882055+VitaminC1000@users.noreply.github.com>
2024-04-11 20:48:55 +0900

    Update 치킨 배달.py

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -6,10 +6,10 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 city = [list(map(int, input().split())) for _ in range(n)]
 
-house = [(i, j) for i in range(n) for j in range(n) if city[i][j] == 1]
-chicken = [(i, j) for i in range(n) for j in range(n) if city[i][j] == 2]
+house = [(i, j) for i in range(n) for j in range(n) if city[i][j] == 1] # 모든 집
+chicken = [(i, j) for i in range(n) for j in range(n) if city[i][j] == 2] # 모든 치킨 집
 
-m_chicken = combinations(chicken, m)
+m_chicken = combinations(chicken, m) # m개의 조합 생성
 
 minimum = 1e10
 
@@ -22,15 +22,15 @@ def check(current_chicken):
         temp = 1e10
         
         for c in current_chicken:
-            l = abs(h[0] - c[0]) + abs(h[1] - c[1])
-            temp = min(temp, l)
+            l = abs(h[0] - c[0]) + abs(h[1] - c[1]) # 현재 집과 치킨 집의 치킨 거리
+            temp = min(temp, l) # 이전 결과보다 작으면 포함
 
-        ans += temp     
+        ans += temp # 각 집의 치킨 거리 추가 (도시의 치킨 거리)
     
     return ans
 
 for c in m_chicken:
     ans = check(c)
-    minimum = min(minimum, ans)
+    minimum = min(minimum, ans) # 도시의 치킨 거리 중 최소값 저장
     
 print(minimum)
